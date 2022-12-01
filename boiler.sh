93a41fbd68bfc30b4c3909f2d4aff9b26bbf8fcc
sebastianselander <sebastian.selander@gmail.com>
2022-12-01 17:22:13 +0100

    regex replace in main

--- boiler.sh
+++ boiler.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo "Provide a number for the day to set up"
-read DAY
+read DAYNUMBER
 
+PROJECT=$HOME/Documents/git/aoc22
 DIR=$HOME/Documents/git/aoc22/src/Day$DAYNUMBER
 FILE=$HOME/Documents/git/aoc22/src/Day$DAYNUMBER/Day$DAYNUMBER.hs
 TEMPLATE=$HOME/Documents/git/aoc22/src/Template.hs
@@ -14,10 +15,12 @@ else
     else
         mkdir $DIR
         touch $FILE
-        curl --cookie ~/Downloads/aocookies.txt https://adventofcode.com/2022/day/$DAYNUMBER/input >> $HOME/Documents/git/aoc22/src/Day$DAYNUMBER/input.txt
+        # curl --cookie ~/Downloads/aocookies.txt https://adventofcode.com/2022/day/$DAYNUMBER/input >> $HOME/Documents/git/aoc22/src/Day$DAYNUMBER/input.txt
         cp $TEMPLATE $DIR/Day$DAYNUMBER.hs
         sed -i "s/dayNumber = 0/dayNumber = $DAYNUMBER/g" $FILE
         sed -i "s/module Template/module Day$DAYNUMBER.Day$DAYNUMBER/g" $FILE
+        sed -i "s/Day\d*.Day\d*/Day$DAYNUMBER.Day$DAYNUMBER/g" $PROJECT/app/Main.hs
+        sed -i "s/import Day.*\.Day.* qualified as D/import Day$DAYNUMBER.Day$DAYNUMBER qualified as D/g" $PROJECT/app/Main.hs
         echo "\n Files generated!"
     fi
 fi
