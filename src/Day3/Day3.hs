f96f2305ae55416ff96f993b5bf83bc8c10d2e86
sebastianselander <sebastian.selander@gmail.com>
2022-12-03 12:07:30 +0100

    golf'd a bit

--- src/Day3/Day3.hs
+++ src/Day3/Day3.hs
@@ -3,35 +3,29 @@ module Day3.Day3
   , solve2
   ) where
 
-import System.IO.Unsafe
-
-import Data.List
+import Control.Arrow ((&&&))
+import Data.Bool (bool)
 import Misc
 
-input :: String
-input = unsafePerformIO $ readFile "Day3/test.txt"
-
-parseItem1 :: String -> (String, String)
-parseItem1 item = splitAt middle item
-  where
-    middle = length item `div` 2
-
-commons :: (String, String) -> String
-commons (i1, i2) = nub $ intersect i1 i2
-
-pointify :: Char -> Int
-pointify c
-  | isUpper c = ord c - 38
-  | otherwise = ord c - 96
-
 solve1 :: String -> String
-solve1 = show . sum . concatMap (map pointify . commons . parseItem1) . lines
+solve1 =
+  show .
+  sum .
+  concatMap
+    (map (\c -> bool (ord c - 96) (ord c - 38) (isUpper c)) .
+     nub . uncurry intersect . uncurry splitAt . (flip div 2 . length &&& id)) .
+  lines
 
 solve2 :: String -> String
-solve2 = show . sum . concatMap ((map pointify) . nub . common) . split . lines
+solve2 =
+  show .
+  sum .
+  concatMap
+    (map (\c -> bool (ord c - 96) (ord c - 38) (isUpper c)) . nub . common) .
+  split . lines
   where
     split :: [String] -> [[String]]
     split [] = []
     split (a:b:c:rest) = [a, b, c] : split rest
     common :: [String] -> String
-    common (a:b:c:[]) = intersect (intersect a b) c
+    common (a:b:c:_) = intersect (intersect a b) c
--- src/Misc.hs
+++ src/Misc.hs
@@ -1,7 +1,9 @@
 module Misc
   ( module Data.List.Split
   , module Data.Char
+  , module Data.List
   ) where
 
 import Data.Char
+import Data.List
 import Data.List.Split
