aff0a237902507a2728b3643ad2e80748d4e676a
sebastianselander <sebastian.selander@gmail.com>
2022-12-05 16:08:54 +0100

    right associativity fucked me over

--- src/Day5/Day5.hs
+++ src/Day5/Day5.hs
@@ -23,21 +23,28 @@ data Instr = Instr {
 
 parseInstr :: String -> [Instr]
 parseInstr = toInstr . wordsBy (not . isDigit) . concat . drop 10 . lines
-    where
-      toInstr :: [String] -> [Instr]
-      toInstr []         = []
-      toInstr (a:b:c:xs) = Instr (read a) (read b) (read c) : toInstr xs
-      toInstr _          = error "failed to parse"
 
-runInstr :: Instr -> M.Map Int String -> M.Map Int String
-runInstr instr crates = crates'
+toInstr :: [String] -> [Instr]
+toInstr []         = []
+toInstr (a:b:c:xs) = Instr (read a) (read b) (read c) : toInstr xs
+toInstr _          = error "failed to parse"
+
+runInstr1 :: Instr -> M.Map Int String -> M.Map Int String
+runInstr1 instr crates = crates'
     where
     (Just froms)  = M.lookup (from instr) crates
-    (Just tos)  = M.lookup (to instr) crates
-    crates'     = M.insert (to instr) ((take (amount instr) froms) ++ tos) $ M.insert (from instr) (drop (amount instr) froms) crates
+    (Just tos)    = M.lookup (to instr) crates
+    crates'       = M.insert (from instr) (drop (amount instr) froms) $ M.insert (to instr) (reverse (take (amount instr) froms) ++ tos) crates
+
+runInstr2 :: Instr -> M.Map Int String -> M.Map Int String
+runInstr2 instr crates = crates'
+    where
+    (Just froms)  = M.lookup (from instr) crates
+    (Just tos)    = M.lookup (to instr) crates
+    crates'       = M.insert (from instr) (drop (amount instr) froms) $ M.insert (to instr) ((take (amount instr) froms) ++ tos) crates
 
 -- solve1 :: String -> String
-solve1 str = foldr runInstr (parseCrates str) (parseInstr str)
+solve1 str = map (head . snd) $ M.toList $ foldl' (\acc x -> runInstr1 x acc) (parseCrates str) (parseInstr str)
 
 solve2 :: String -> String
-solve2 = const "unsolved"
+solve2 str = map (head . snd) $ M.toList $ foldl' (\acc x -> runInstr2 x acc) (parseCrates str) (parseInstr str)
