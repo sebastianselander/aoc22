f180ca765b32622c9d964844492dd5d41e6e4dfb
sebastianselander <sebastian.selander@gmail.com>
2022-12-05 16:44:12 +0100

    pointfreeing

--- src/Day5/Day5.hs
+++ src/Day5/Day5.hs
@@ -4,43 +4,46 @@ module Day5.Day5
   ) where
 
 import Misc
-import System.IO.Unsafe (unsafePerformIO)
 import Data.Map qualified as M
+import Control.Arrow (( &&& ))
 
-parseCrates :: String -> M.Map Int String
-parseCrates = M.fromList . zip [1..9] . filter (not . null) . map (filter isUpper) . transpose . take 8 . lines
-
-data Instr = Instr {
-                     amount :: Int
+data Instr = Instr { amount :: Int
                    , from :: Int
                    , to :: Int
                    }
     deriving Show
-
-parseInstr :: String -> [Instr]
-parseInstr = toInstr . wordsBy (not . isDigit) . concat . drop 10 . lines
 
 toInstr :: [String] -> [Instr]
 toInstr []         = []
 toInstr (a:b:c:xs) = Instr (read a) (read b) (read c) : toInstr xs
 toInstr _          = error "failed to parse"
 
-runInstr1 :: Instr -> M.Map Int String -> M.Map Int String
-runInstr1 instr crates = crates'
-    where
-    (Just froms)  = M.lookup (from instr) crates
-    (Just tos)    = M.lookup (to instr) crates
-    crates'       = M.insert (from instr) (drop (amount instr) froms) $ M.insert (to instr) (reverse (take (amount instr) froms) ++ tos) crates
+parseCrates :: String -> M.Map Int String
+parseCrates = M.fromList
+            . zip [1..9]
+            . filter (not . null)
+            . map (filter isUpper)
+            . transpose
+            . take 8
+            . lines
 
-runInstr2 :: Instr -> M.Map Int String -> M.Map Int String
-runInstr2 instr crates = crates'
+
+parseInstr :: String -> [Instr]
+parseInstr = toInstr
+           . wordsBy (not . isDigit)
+           . concat
+           . drop 10
+           . lines
+
+runInstr :: (String -> String) -> Instr -> M.Map Int String -> M.Map Int String
+runInstr f instr crates = updateCrate crates
     where
-    (Just froms)  = M.lookup (from instr) crates
-    (Just tos)    = M.lookup (to instr) crates
-    crates'       = M.insert (from instr) (drop (amount instr) froms) $ M.insert (to instr) ((take (amount instr) froms) ++ tos) crates
+    froms = fromMaybe (error "fail") $ M.lookup (from instr) crates
+    tos = fromMaybe (error "fail") $ M.lookup (to instr) crates
+    updateCrate = M.insert (from instr) (drop (amount instr) froms) . M.insert (to instr) (f (take (amount instr) froms) ++ tos)
 
 solve1 :: String -> String
-solve1 str = map (head . snd) $ M.toList $ foldl' (\acc x -> runInstr1 x acc) (parseCrates str) (parseInstr str)
+solve1 = map (head . snd) . M.toList . uncurry (foldl' (flip (runInstr reverse))) . (parseCrates &&& parseInstr)
 
 solve2 :: String -> String
-solve2 str = map (head . snd) $ M.toList $ foldl' (\acc x -> runInstr2 x acc) (parseCrates str) (parseInstr str)
+solve2 = map (head . snd) . M.toList . uncurry (foldl' (flip (runInstr id))) . (parseCrates &&& parseInstr)
--- src/Misc.hs
+++ src/Misc.hs
@@ -1,19 +1,21 @@
 module Misc
-  (  module Data.List.Split
+  ( module Data.List.Split
   , module Data.Char
   , module Data.List
   , module Data.Function
+  , module Data.Maybe
   , fullyContained
   , overlaps
   ) where
 
 import Data.Char
+import Data.Function
 import Data.List
 import Data.List.Split
-import Data.Function
+import Data.Maybe
 
 fullyContained :: Ord a => (a, a) -> (a, a) -> Bool
-fullyContained (a,b) (c,d) = (a <= c && b >= d) || (c <= a && d >= b)
+fullyContained (a, b) (c, d) = (a <= c && b >= d) || (c <= a && d >= b)
 
 overlaps :: Ord a => (a, a) -> (a, a) -> Bool
-overlaps (a,b) (c,d) = a <= c && b >= c || a >= c && a <= d
+overlaps (a, b) (c, d) = a <= c && b >= c || a >= c && a <= d
