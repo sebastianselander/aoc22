c3714e555617df388b7a1c2713380e5e2761981f
Sebastian Selander <sebastian.selander@gmail.com>
2022-12-06 22:26:29 +0100

    janitor at work

--- src/Day6/Day6.hs
+++ src/Day6/Day6.hs
@@ -5,15 +5,14 @@ module Day6.Day6
 
 import Misc
 
-check :: Int -> Int -> String -> Int
-check count amount xs =
-    bool
-        (check (count+1) amount (tail xs))
-        count
-        ((length $ nub $ take amount xs) == amount)
+check :: Int -> String -> Int
+check amount xs = bool
+        (1 + check amount (tail xs))
+        amount
+        ((== amount) . length . nub . take amount $ xs)
 
 solve1 :: String -> String
-solve1 = show . check 4 4
+solve1 = show . check 4
 
 solve2 :: String -> String
-solve2 = show . check 14 14
+solve2 = show . check 14
