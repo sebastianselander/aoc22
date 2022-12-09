fec31885296dd1b668d26b9524df87b975cb2c62
sebastianselander <sebastian.selander@gmail.com>
2022-12-09 22:03:26 +0100

    janitorium

--- src/Day9/Day9.hs
+++ src/Day9/Day9.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE LambdaCase #-}
 module Day9.Day9
   ( solve1
   , solve2
   ) where
 
 import Misc
+import Data.Bifunctor (first, second)
 
-data Motion = L | U | R | D
+data Motion = D | L | U | R
     deriving Show
 
 parse :: String -> [Motion]
@@ -16,20 +16,24 @@ parse = toMotion . lines
       toMotion = \case
         [] -> []
         (x:xs) -> case x of
-            ('L':' ':n) -> if read n == 1 then L : toMotion xs
-                                          else L : toMotion (("L " ++ (show $ (read n) - 1)) : xs)
-            ('R':' ':n) -> if read n == 1 then R : toMotion xs
-                                          else R : toMotion (("R " ++ (show $ (read n) - 1)) : xs)
-            ('U':' ':n) -> if read n == 1 then U : toMotion xs
-                                          else U : toMotion (("U " ++ (show $ (read n) - 1)) : xs)
-            ('D':' ':n) -> if read n == 1 then D : toMotion xs
-                                          else D : toMotion (("D " ++ (show $ (read n) - 1)) : xs)
+            ('L':' ':n) -> if read n == 1
+                             then L : toMotion xs
+                             else L : toMotion (("L " ++ (show $ (read n) - 1)) : xs)
+            ('R':' ':n) -> if read n == 1
+                             then R : toMotion xs
+                             else R : toMotion (("R " ++ (show $ (read n) - 1)) : xs)
+            ('U':' ':n) -> if read n == 1
+                             then U : toMotion xs
+                             else U : toMotion (("U " ++ (show $ (read n) - 1)) : xs)
+            ('D':' ':n) -> if read n == 1
+                             then D : toMotion xs
+                             else D : toMotion (("D " ++ (show $ (read n) - 1)) : xs)
 
-move :: (Int,Int) -> Motion -> (Int,Int)
-move (x,y) R = (x+1,y)
-move (x,y) L = (x-1,y)
-move (x,y) U = (x,y+1)
-move (x,y) D = (x,y-1)
+move ::  Motion ->  (Int,Int) ->(Int,Int)
+move R = first (+1)
+move L = first (flip (-) 1)
+move U = second (+1)
+move D = second (flip (-) 1)
 
 tooFar :: (Int,Int) -> (Int,Int) -> Bool
 tooFar (tx,ty) (hx,hy) = abs (hx - tx) > 1 || abs (hy - ty) > 1
@@ -39,7 +43,7 @@ hdmvnt = (:) (0,0) . go (0,0)
   where
     go :: (Int,Int) -> [Motion] -> [(Int,Int)]
     go hd [] = []
-    go hd (x:xs) = let newHead = move hd x
+    go hd (x:xs) = let newHead = move x hd
                    in newHead : go newHead xs
 
 tailFollow :: (Int,Int) -> (Int,Int) -> (Int,Int)
@@ -47,32 +51,32 @@ tailFollow (tx,ty) (hx,hy) =
     -- Surely this can be written in a better way
     case (hx - tx, hy - ty) of
 
-        (2,2) -> (tx+1,ty+1)
-        (1,2) -> (tx+1,ty+1)
-        (2,1) -> (tx+1,ty+1)
+        (2,2)   -> (tx+1,ty+1)
+        (1,2)   -> (tx+1,ty+1)
+        (2,1)   -> (tx+1,ty+1)
 
-        (2,-2) -> (tx+1,ty-1)
-        (1,-2) -> (tx+1,ty-1)
-        (2,-1) -> (tx+1,ty-1)
+        (2,-2)  -> (tx+1,ty-1)
+        (1,-2)  -> (tx+1,ty-1)
+        (2,-1)  -> (tx+1,ty-1)
 
-        (0,2) -> (tx,ty+1)
-        (0,-2) -> (tx,ty-1)
-        (2,0) -> (tx+1,ty)
-        (-2,0) -> (tx-1,ty)
-
-        (-2,2) -> (tx-1,ty+1)
-        (-1,2) -> (tx-1,ty+1)
-        (-2,1) -> (tx-1,ty+1)
+        (-2,2)  -> (tx-1,ty+1)
+        (-1,2)  -> (tx-1,ty+1)
+        (-2,1)  -> (tx-1,ty+1)
 
         (-2,-2) -> (tx-1,ty-1)
         (-1,-2) -> (tx-1,ty-1)
         (-2,-1) -> (tx-1,ty-1)
 
+        (0,2)   -> (tx,ty+1)
+        (0,-2)  -> (tx,ty-1)
+        (2,0)   -> (tx+1,ty)
+        (-2,0)  -> (tx-1,ty)
+
         _       -> (tx,ty)
 
 tailMovement :: (Int,Int) -> [(Int,Int)] -> [(Int,Int)]
 tailMovement tl [cur] = if tooFar tl cur then [cur] else []
-tailMovement tl (prev:cur:xs) = newTail : tailMovement newTail (cur:xs)
+tailMovement tl (cur:xs) = newTail : tailMovement newTail xs
     where
       newTail = (tailFollow tl cur)
 
