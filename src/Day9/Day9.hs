d993f3fb710abda66b53b55fbede4002d2ac0ff9
sebastianselander <sebastian.selander@gmail.com>
2022-12-09 21:38:48 +0100

    janitor

--- src/Day9/Day9.hs
+++ src/Day9/Day9.hs
@@ -4,21 +4,11 @@ module Day9.Day9
   , solve2
   ) where
 
-import Prelude hiding (Either, Left, Right)
-import System.IO.Unsafe (unsafePerformIO)
 import Misc
-import Debug.Trace
 
-ex0 = "R 4\nU 4\nL 3\nD 1\nR 4\nD 1\nL 5\nR 2"
-
-ex1 = "R 5\nU 8\nL 8\nD 3\nR 17\nD 10\nL 25\nU 20"
-
-
-
-inp = unsafePerformIO $ readFile "src/Day9/input.txt"
-
-data Motion = Left | Up | Right | Down
+data Motion = L | U | R | D
     deriving Show
+
 parse :: String -> [Motion]
 parse = toMotion . lines
     where
@@ -26,76 +16,80 @@ parse = toMotion . lines
       toMotion = \case
         [] -> []
         (x:xs) -> case x of
-            ('L':' ':n) -> if read n == 1 then Left : toMotion xs
-                                          else Left : toMotion (("L " ++ (show $ (read n) - 1)) : xs)
-            ('R':' ':n) -> if read n == 1 then Right : toMotion xs
-                                          else Right : toMotion (("R " ++ (show $ (read n) - 1)) : xs)
-            ('U':' ':n) -> if read n == 1 then Up : toMotion xs
-                                          else Up : toMotion (("U " ++ (show $ (read n) - 1)) : xs)
-            ('D':' ':n) -> if read n == 1 then Down : toMotion xs
-                                          else Down : toMotion (("D " ++ (show $ (read n) - 1)) : xs)
+            ('L':' ':n) -> if read n == 1 then L : toMotion xs
+                                          else L : toMotion (("L " ++ (show $ (read n) - 1)) : xs)
+            ('R':' ':n) -> if read n == 1 then R : toMotion xs
+                                          else R : toMotion (("R " ++ (show $ (read n) - 1)) : xs)
+            ('U':' ':n) -> if read n == 1 then U : toMotion xs
+                                          else U : toMotion (("U " ++ (show $ (read n) - 1)) : xs)
+            ('D':' ':n) -> if read n == 1 then D : toMotion xs
+                                          else D : toMotion (("D " ++ (show $ (read n) - 1)) : xs)
 
 move :: (Int,Int) -> Motion -> (Int,Int)
-move (x,y) Right = (x+1,y)
-move (x,y) Left = (x-1,y)
-move (x,y) Up = (x,y+1)
-move (x,y) Down = (x,y-1)
+move (x,y) R = (x+1,y)
+move (x,y) L = (x-1,y)
+move (x,y) U = (x,y+1)
+move (x,y) D = (x,y-1)
 
-tailTooFar :: (Int,Int) -> (Int,Int) -> Bool
-tailTooFar (tx,ty) (hx,hy) = abs (hx - tx) > 1 || abs (hy - ty) > 1
-
-tailFollow :: (Int,Int) -> (Int,Int) -> (Int,Int) -> (Int,Int)
-tailFollow tlx hprev hcur = bool tlx hprev (tailTooFar tlx hcur)
+tooFar :: (Int,Int) -> (Int,Int) -> Bool
+tooFar (tx,ty) (hx,hy) = abs (hx - tx) > 1 || abs (hy - ty) > 1
 
 hdmvnt :: [Motion] -> [(Int,Int)]
-hdmvnt = (:) (0,0) . headMovement (0,0)
+hdmvnt = (:) (0,0) . go (0,0)
   where
-    headMovement :: (Int,Int) -> [Motion] -> [(Int,Int)]
-    headMovement hd [] = []
-    headMovement hd (x:xs) = newHead : headMovement newHead xs
-        where
-          newHead = move hd x
+    go :: (Int,Int) -> [Motion] -> [(Int,Int)]
+    go hd [] = []
+    go hd (x:xs) = let newHead = move hd x
+                   in newHead : go newHead xs
 
-tailMovement :: (Int,Int) -> [(Int,Int)] -> [(Int,Int)]
-tailMovement tl [cur] = if tailTooFar tl cur then [cur] else []
-tailMovement tl (prev:cur:xs) = newTail : tailMovement newTail (cur:xs)
-    where
-      newTail = (tailFollow tl prev cur)
-
-tailFollow' :: (Int,Int) -> (Int,Int) -> (Int,Int)
-tailFollow' (tx,ty) (hx,hy) =
+tailFollow :: (Int,Int) -> (Int,Int) -> (Int,Int)
+tailFollow (tx,ty) (hx,hy) =
+    -- Surely this can be written in a better way
     case (hx - tx, hy - ty) of
+
         (2,2) -> (tx+1,ty+1)
-        (-2,-2) -> (tx-1,ty-1)
+        (1,2) -> (tx+1,ty+1)
+        (2,1) -> (tx+1,ty+1)
+
         (2,-2) -> (tx+1,ty-1)
-        (-2,2) -> (tx-1,ty+1)
+        (1,-2) -> (tx+1,ty-1)
+        (2,-1) -> (tx+1,ty-1)
 
         (0,2) -> (tx,ty+1)
         (0,-2) -> (tx,ty-1)
         (2,0) -> (tx+1,ty)
         (-2,0) -> (tx-1,ty)
 
-        (1,2) -> (tx+1,ty+1)
-        (2,1) -> (tx+1,ty+1)
-        (1,-2) -> (tx+1,ty-1)
+        (-2,2) -> (tx-1,ty+1)
+        (-1,2) -> (tx-1,ty+1)
         (-2,1) -> (tx-1,ty+1)
 
-        (-1,2) -> (tx-1,ty+1)
-        (2,-1) -> (tx+1,ty-1)
+        (-2,-2) -> (tx-1,ty-1)
         (-1,-2) -> (tx-1,ty-1)
         (-2,-1) -> (tx-1,ty-1)
+
         _       -> (tx,ty)
 
-tailMovement' :: (Int,Int) -> [(Int,Int)] -> [(Int,Int)]
-tailMovement' tl [cur] = if tailTooFar tl cur then [cur] else []
-tailMovement' tl (prev:cur:xs) = newTail : tailMovement' newTail (cur:xs)
+tailMovement :: (Int,Int) -> [(Int,Int)] -> [(Int,Int)]
+tailMovement tl [cur] = if tooFar tl cur then [cur] else []
+tailMovement tl (prev:cur:xs) = newTail : tailMovement newTail (cur:xs)
     where
-      newTail = (tailFollow' tl cur)
-
-sim = nub . last . take 10 . iterate (tailMovement' (0,0)) . hdmvnt . parse
+      newTail = (tailFollow tl cur)
 
 solve1 :: String -> String
-solve1 = show . length . nub . tailMovement (0,0) . hdmvnt . parse
+solve1 = show
+       . length
+       . nub
+       . tailMovement (0,0)
+       . hdmvnt
+       . parse
 
 solve2 :: String -> String
-solve2 = show . length . sim
+solve2 = show
+       . length
+       . nub
+       . last
+       . take 10
+       . iterate (tailMovement (0,0))
+       . hdmvnt
+       . parse
