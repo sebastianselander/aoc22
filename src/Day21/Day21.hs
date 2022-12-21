b26b88910ec8e1fd58c4303315ad7b0ddddd4e87
sebastianselander <sebastian.selander@gmail.com>
2022-12-21 13:28:48 +0100

    day21 part 1

---
+++ src/Day21/Day21.hs
@@ -0,0 +1,14 @@
+module Day21.Day21
+  ( solve1
+  , solve2
+  ) where
+
+import Misc
+
+
+--rename input file to input.hs, search and replace `:` with `=`
+solve1 :: String -> IO ()
+solve1 = putStrLn "Open ghci with input.hs and run `root`"
+
+solve2 :: String -> IO ()
+solve2 = putStrLn "unsolved"
